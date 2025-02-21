899cd591bf5bfd1bf52ab89f0e98c09fb3728284
Sajjad Pirmohammadi <pb.sajjad@gmail.com>
2025-02-21 18:49:15 +0330

    Render a basic 4x4 puzzle layout

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,7 @@
-console.log("JavaScript is here!");
+import { PuzzleBoard } from "./puzzle-board.js";
+
+const puzzleBoardElement = document.getElementById("puzzle-board");
+const BOARD_SIZE = 4;
+const puzzleBoard = new PuzzleBoard(BOARD_SIZE, puzzleBoardElement);
+
+puzzleBoard.init();
