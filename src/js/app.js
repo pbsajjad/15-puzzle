583f90a36ce5e9388be756b5302c95f528d9bf85
Sajjad Pirmohammadi <pb.sajjad@gmail.com>
2025-02-23 01:14:27 +0330

    Initialize app with game instance

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,5 @@
-import { PuzzleBoard } from "./puzzle-board.js";
+import { Game } from "./game.js";
 
-const puzzleBoardElement = document.getElementsByClassName("puzzle-grid")[0];
-const puzzleBoardMessageElement =
-  document.getElementsByClassName("message-box")[0];
-const NUMBER_OF_ROWS = 3;
-const NUMBER_OF_COLS = 3;
-const TILE_SIZE_IN_PX = 100;
-const puzzleBoard = new PuzzleBoard(
-  NUMBER_OF_ROWS,
-  NUMBER_OF_COLS,
-  TILE_SIZE_IN_PX,
-  puzzleBoardElement,
-  puzzleBoardMessageElement
-);
+const game = new Game();
 
-puzzleBoard.init();
+game.init();
